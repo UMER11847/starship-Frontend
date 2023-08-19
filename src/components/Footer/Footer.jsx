471b36e9fb9ed6a7dd98c3d6dbee5a7d019876fb
umer <m.umer4058umer@gmail.com>
2023-08-19 14:36:58 -0700

    created Footer and News letter section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
-
+import React from "react";
+import "./Footer.scss";
+import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
+import Payment from "../../assets/payments.png";
 const Footer = () => {
-  return (
-    <div>
-      <h1>home</h1>
-    </div>
-  )
-}
+    return (
+        <div className="footer">
+            <div className="footer-content">
+                <div className="col">
+                    <div className="title">About</div>
+                    <div className="text">
+                    Discover a realm of cutting-edge possibilities at our tech store, 
+                    where innovation and imagination converge to redefine your digital journey.
+                    We have every item related to tech at amzaing price tag.
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="title">Contact</div>
+                    <div className="c-item">
+                        <FaLocationArrow />
+                        <div className="text">
+                        Mr Umer, Mr Asil. 132, My Street, Kingston, New York 12401. United States.
+                        </div>
+                    </div>
+                    <div className="c-item">
+                        <FaMobileAlt />
+                        <div className="text">Phone: 001-002-003</div>
+                    </div>
+                    <div className="c-item">
+                        <FaEnvelope />
+                        <div className="text">Email: tech@store.com</div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="title">Categories</div>
+                    <span className="text">Headphones</span>
+                    <span className="text">Smart Watches</span>
+                    <span className="text">Bluetooth Speakers</span>
+                    <span className="text">Wireless Earbuds</span>
+                    <span className="text">Home Theatre</span>
+                    <span className="text">Laptops</span>
+                </div>
+                <div className="col">
+                    <div className="title">Pages</div>
+                    <span className="text">Home</span>
+                    <span className="text">About</span>
+                    <span className="text">Privacy Policy</span>
+                    <span className="text">Returns</span>
+                    <span className="text">Terms & Conditions</span>
+                    <span className="text">Contact Us</span>
+                </div>
+            </div>
+            <div className="bottom-bar">
+                <div className="bottom-bar-content">
+                    <span className="text">
+                        TECHSTORE 2023 CREATED BY UMER & ASIL. PREMIUM E-COMMERCE
+                        SOLUTIONS.
+                    </span>
+                    <img src={Payment} />
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Footer
+export default Footer;
