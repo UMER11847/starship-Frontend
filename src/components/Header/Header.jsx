45fc94fe2d8558bfef385840a340c65e2a90ffaf
umer <m.umer4058umer@gmail.com>
2023-08-19 05:39:23 -0700

    created navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 
+import { CgShoppingCart} from "react-icons/cg"
+import { AiOutlineHeart} from "react-icons/ai"
+import { TbSearch} from "react-icons/tb"
+import "./Header.scss"
+
+import{useEffect, useState} from "react"
+
 const Header = () => {
+const[scrolled, setScrolled] = useState(false)
+const ScrollHandler = ()=>{
+  const offSet = window.scrollY
+  if(offSet > 200){
+    setScrolled(true)
+  }else{
+    setScrolled(false)
+  }
+}
+
+useEffect( ()=>{
+  window.addEventListener("scroll", ScrollHandler)
+})
+
   return (
-    <div>
-      <h1>header</h1>
-    </div>
+    <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+      <div className='header-content'>
+        <ul className='left'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Categories</li>
+        </ul>
+      <div className="center">TECH STORE</div>
+      <div className="right">
+        <TbSearch />
+        <AiOutlineHeart />
+        <span className='cart-icon'>
+          <CgShoppingCart />
+          <span>6</span>
+        </span>
+      </div>
+      </div>
+    </header>
   )
 }
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import "./Home.scss"
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className='home'>Home</h1>
     </div>
   )
 }
