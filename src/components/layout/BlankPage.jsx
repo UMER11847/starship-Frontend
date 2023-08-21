13b9ab57c1b0b28325445cd0a95c994a198dbb9b
DefkieHaust <noreply>
2023-08-22 02:00:47 +0500

    fixed mui

--- src/components/layout/BlankPage.jsx
+++ src/components/layout/BlankPage.jsx
@@ -1,9 +1,22 @@
+// Components
+import { Container } from "@mui/material"
+// Styling
+import "./scss/BlankPage.scss"
+// Assets
+import mask from "../../assets/BlankPage/auth-v1-mask-light.png"
+import plant1 from "../../assets/BlankPage/auth-v1-tree.png"
+import plant2 from "../../assets/BlankPage/auth-v1-tree-2.png"
 
 const BlankPage = ({ children }) => {
   return (
-    <div>
+    <>
+      <Container maxWidth="sm">
         {children}
-    </div>
+      </Container>
+      <img className="plant1" src={plant1} />
+      <img className="plant2" src={plant2}/>
+      <img className="mask" src={mask}/>
+    </>
   )
 }
 
